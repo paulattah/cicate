b99bf2e5bffef315a2ef3737f6991fc1a21386e6
Paul attah <paulattah@paul.local>
2023-12-19 11:41:38 +0100

    added cors header

--- cicate/cicate/settings.py
+++ cicate/cicate/settings.py
@@ -40,11 +40,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    "corsheaders",
     'rest_framework',
     'users',
 ]
 
 MIDDLEWARE = [
+
+    "corsheaders.middleware.CorsMiddleware",
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
